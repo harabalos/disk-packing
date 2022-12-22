31c3508cad2442720b6d87ee79c7d2bd693dae75
harabalos <babismetaxas000@gmail.com>
2022-12-22 13:43:11 +0200

    Made the Disk class

--- src/Disk.java
+++ src/Disk.java
@@ -1,3 +1,39 @@
-public class Disk {
+
+public class Disk implements Comparable<Disk>{
+
+    private int id;
+    private int initialspace;
+    private SinglyLinkedList<Integer> folders=new SinglyLinkedList<Integer>(); 
+
+    public Disk() { 
+        this.id = 0;             
+        this.initialspace=1000000; 
+        this.folders=new SinglyLinkedList<Integer>();
+    }
+
+    public int getFreeSpace() { 
+        return this.initialspace;
+    }
     
-}
+
+    public int getId(){ 
+        return this.id;
+    }
+
+
+    public SinglyLinkedList<Integer> getFolders(){ 
+        return this.folders;
+    }
+
+    public int compareTo(Disk o) {
+        if(getFreeSpace()>o.getFreeSpace()){
+            return 1;
+        }else if(getFreeSpace()<o.getFreeSpace()){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+
+    }
