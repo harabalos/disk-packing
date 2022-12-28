d962bbb0d44a96f908333b11dd21d2990886c776
harabalos <babismetaxas000@gmail.com>
2022-12-28 12:32:12 +0200

    Added comments

--- src/Sort.java
+++ src/Sort.java
@@ -4,30 +4,43 @@ import java.util.Scanner;
 
 public class Sort {
     public static void main(String[] args) throws FileNotFoundException {
+
+        //Imports the fixed SinglyLinkedList from Greedy class
         Scanner scan=new Scanner(new File(args[0]));
+        //Executes Greedy algorithm with the decreasingly sorted inputfolderlist
+
         SinglyLinkedList<Integer> mblist = Greedy.readFile(scan);
         SinglyLinkedList<Disk> diskList = sortExecute(mblist);
         MaxPQ folderList = Greedy.insertMaxPq(diskList);
+        
         Greedy.printResult(diskList, folderList);
 
     }
 
     public static SinglyLinkedList<Disk> sortExecute(SinglyLinkedList<Integer> inputfolderlist){
 
+        //Transfers the inputfolderlist's data to an array so the sort can begin
         Integer[] aux= new Integer[inputfolderlist.length()];
         for (int k = 0; k < aux.length; k++) {
             aux[k]=inputfolderlist.get(k);
         }
 
+        //Merge Sort     
         mergesort(aux, 0, aux.length-1);
 
+        //Re trasnfer the sorted elements to a new SinglyLinkedList
         SinglyLinkedList<Integer> inputfolderlist1=new SinglyLinkedList<Integer>();
         for (int k =0; k < aux.length; k++) {
             inputfolderlist1.add(aux[k]);
         }
+
+        //Executes Greedy algorithm with the decreasingly sorted inputfolderlist
+
         return Greedy.greedyMethod(inputfolderlist1);
     }
 
+
+    //Merge Sort
     static void mergesort(Integer[] aux,int first,int last){
         if (last <= first) return;
         for (int m = 1; m <= last-first; m = m+m){
@@ -36,6 +49,9 @@ public class Sort {
             }
         }   
     } 
+
+
+    //Array merging
     static void merge(Integer[] aux,int first,int middle,int last){
         Integer [] leftArray=new Integer[middle-first+1];
         Integer [] rightArray=new Integer[last-middle];
@@ -74,7 +90,8 @@ public class Sort {
             }
         }
     }
-
+    
+    //Min finding
     static int min(int A, int B) {
         return (A < B)? A: B; 
     }
